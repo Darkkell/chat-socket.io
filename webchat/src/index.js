a029afa14b580ebec9058915cdb96a755bd1057a
Fermín Alejandro Flores Reyes <alejandro.flores.reyes@outlook.com>
2022-08-20 12:23:50 -0500

    base project

---
+++ webchat/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createServer } = require('http');
+const realtimeServer = require('./realtimeServer');
+const path = require('path');
+
+const app = express();
+const httpServer = createServer(app);
+
+// settings
+app.set('port', process.env.PORT || 9300);
+app.set('views', path.join(__dirname, 'views'));
+
+// routes
+app.use(require('./routes'));
+
+//public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// server on
+httpServer.listen(app.get('port'), ()=>{
+    console.log('listening on ', app.get('port'));
+});
+
+// call server socket.io
+realtimeServer(httpServer);
